550f9d678443ab630227ad7e1ae7b5bedd02ceac
phyxius23 <30ntin@gmail.com>
2023-04-02 17:51:11 +0200

    added a comment on how to view the profile page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
-import HeaderHomepage from './components/HeaderHomepage';
-import MyFooter from './components/MyFooter';
 import MyNav from './components/MyNav';
 // import Profile from './components/AccountPage';
+import HeaderHomepage from './components/HeaderHomepage';
 import ShowFilms from './components/ShowFilms';
+import MyFooter from './components/MyFooter';
 
 function App() {
   return (
     <>
+     {/* 
+       Per visualizzare la pagina profilo in modo corretto devo:
+       1 - alla props di MyNav devo cambiare il valore in true
+       2 - decommentare il Profile sia tra gli import che nei componenti sotto
+       3 - commentare HeaderHomepage, ShowFilms e MyFooter sia negli import che nei component sotto
+     */}
      <MyNav accountPage="false" />
+     {/* <Profile /> */}
      <HeaderHomepage />
      <ShowFilms search="Harry Potter" />
      <ShowFilms search="Star Wars" />
      <ShowFilms search="Indiana Jones" />
      <MyFooter />
-     {/* <Profile /> */}
     </>
   );
 }
